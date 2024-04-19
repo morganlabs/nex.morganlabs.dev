fa3bd9985bcca0fbd27b50029859554315c0a5d7
Morgan Jones <me@morganlabs.dev>
2024-04-19 10:00:53 +0100

    feat: All under-construction elements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,38 @@
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from "url";
+import { resolve, dirname, join } from "path";
+// import icon from "astro-icon";
+import vercel from "@astrojs/vercel/serverless";
+import icon from "astro-icon";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const src = resolve(__dirname, "src");
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: "hybrid",
+    adapter: vercel(),
+    integrations: [
+        icon()
+    ],
+
+    vite: {
+        resolve: {
+            alias: {
+                "$/": src,
+                $styles: join(src, "styles"),
+                $components: join(src, "components"),
+                $layouts: join(src, "layouts"),
+                $assets: join(src, "assets")
+            }
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `@use "$styles/_vars.scss" as *;`
+                }
+            }
+        }
+    },
+})
